1e227cc72ceb13868e132b14442188ceab00e208
Dylan Weijgertze <dylanwprivate@gmail.com>
2022-08-03 09:06:42 +0200

    removed kindlegen because it can't be downloaded anymore

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -5,67 +5,39 @@ import { basename, dirname } from 'path';
 /**
  * Convert a book to another format
  *
- * @param conversion what conversion command to run
  * @param pathOfFile the path of the file
  * @param filename the name of the file
  * @returns data of conversion
  */
-const convertWith = async (
-    conversion: string,
+const convertToKepub = async (
     pathOfFile: string,
     filename: string
 ): Promise<ConversionData> => {
     const returnValues: ConversionData = {
-        conversion,
+        conversion: 'kepubify',
         filename,
         data: null,
     };
 
-    const outname = pathOfFile.replace(
-        /\.epub$/i,
-        `${conversion === 'kindlegen' ? '.mobi' : '.kepub.epub'}`
-    );
-
-    // commands to run for converters
-    const conversionCommands = {
-        kindlegen: {
-            name: 'kindlegen',
-            commands: [
-                basename(pathOfFile),
-                '-dont_append_source',
-                '-c1',
-                '-o',
-                basename(outname),
-            ],
-        },
-        kepubify: {
-            name: 'kepubify',
-            commands: [
-                '-v',
-                '-u',
-                '-o',
-                basename(outname),
-                basename(pathOfFile),
-            ],
-        },
-    };
+    const outname = pathOfFile.replace(/\.epub$/i, '.kepub.epub');
 
     returnValues.filename = filename
         .replace(/\.kepub\.epub$/i, '.epub')
-        .replace(
-            /\.epub$/i,
-            `${conversion === 'kindlegen' ? '.mobi' : '.kepub.epub'}`
-        );
+        .replace(/\.epub$/i, '.kepub.epub');
 
     returnValues.data = await new Promise((resolve, reject) => {
         // convert book
         let converter;
         try {
             converter = spawn(
-                conversion,
-                conversion === 'kindlegen'
-                    ? conversionCommands.kindlegen.commands
-                    : conversionCommands.kepubify.commands,
+                'kepubify',
+                [
+                    '-v',
+                    '-u',
+                    '-o',
+                    basename(outname),
+                    basename(pathOfFile),
+                ],
                 {
                     stdio: 'inherit',
                     cwd: dirname(pathOfFile),
@@ -84,18 +56,9 @@ const convertWith = async (
                 else console.log('Removed file', pathOfFile);
             });
 
-            // replace epub with mobi8 in name if kindlegen
-            if (conversion === 'kindlegen') {
-                const mobiName = pathOfFile.replace(/\.epub$/i, '.mobi8');
-                unlink(mobiName, (err) => {
-                    if (err) console.error(err);
-                    else console.log(`Removed file: \n ${mobiName}`);
-                });
-            }
-
             // show error code if there is an error
             if (code !== 0) {
-                reject(`${conversion} error code ${code}`);
+                reject(`kepubify error code ${code}`);
                 return;
             }
 
@@ -131,20 +94,8 @@ export const convertBook = async (
         data: null,
     };
 
-    if (mimetype === TYPE_EPUB && agent.includes('Kindle')) {
-        // convert to .mobi
-        convertionData = await convertWith(
-            'kindlegen',
-            pathOfFile,
-            originalFileName
-        );
-    } else if (mimetype === TYPE_EPUB && agent.includes('Kobo') && kepubify) {
-        // convert to Kobo EPUB
-        convertionData = await convertWith(
-            'kepubify',
-            pathOfFile,
-            originalFileName
-        );
+    if (mimetype === TYPE_EPUB && agent.includes('Kobo') && kepubify) {
+        convertionData = await convertToKepub(pathOfFile, originalFileName);
     } else {
         // No conversion
         convertionData.data = pathOfFile;
